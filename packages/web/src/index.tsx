dfcf2f0d6a7304d62fe0a86ae3b20321c8d2011f
Karl <karlwolffgang@googlemail.com>
2020-05-02 14:56:38 +0200

    Fixes for deploy

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,23 +1,27 @@
-import ApolloClient from 'apollo-boost';
-import * as React from 'react';
-import { ApolloProvider } from 'react-apollo';
-import { render } from 'react-dom';
+import ApolloClient from "apollo-boost";
+import * as React from "react";
+import { ApolloProvider } from "react-apollo";
+import { render } from "react-dom";
+import process from "process";
 
-import 'antd/dist/antd.css';
-import 'react-vertical-timeline-component/style.min.css';
-import './styles/main.scss';
+import "antd/dist/antd.css";
+import "react-vertical-timeline-component/style.min.css";
+import "./styles/main.scss";
 
-import App from './App';
+import App from "./App";
 
-const GRAPHQL_API_URL = 'http://localhost:8080/graphql';
+const LOCAL = window.location.origin === "http://localhost:3000";
+const GRAPHQL_API_URL = !LOCAL
+  ? `${window.location.origin}/graphql`
+  : "http://localhost:8080/graphql";
 
 const client = new ApolloClient({
-  uri: GRAPHQL_API_URL
+  uri: GRAPHQL_API_URL,
 });
 
 render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
